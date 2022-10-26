244f4ea5afe5782d095b2dbf848fac8047670ebc
KatiaRassadina <rassadinaekaterina877@gmail.com>
2022-10-26 22:37:47 +0300

    task done

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿// Программа приниает на вход число и выдает сумму цифр в этом числе
+Console.WriteLine("Input an integer number");
+string number = Console.ReadLine();
+int Length = number.Length;
+int result = 0; 
+for (int i = 0; i < Length; i++)
+{
+    result = result + Convert.ToInt32(Convert.ToString(number[i]));
+}
+Console.WriteLine(result);
